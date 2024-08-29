6172fab48f7453ca22fe928bb9104aca1ba84311
29 <791603901@qq.com>
2024-08-29 21:18:04 +0800

    调整定义顺序

--- src/bin/next.rs
+++ src/bin/next.rs
@@ -1,6 +1,41 @@
 use clap::{Parser, Subcommand, ValueEnum};
 use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
 
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    init_tracing_with_ansi();
+
+    match Op::init() {
+        _ => Err(anyhow::anyhow!("暂时todo！")),
+    }
+}
+
+// 参见：https://github.com/tokio-rs/tracing/issues/3068
+fn init_tracing_with_ansi() {
+    #[cfg(windows)]
+    nu_ansi_term::enable_ansi_support().ok();
+    init_tracing();
+}
+
+#[cfg(debug_assertions)]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_max_level(tracing::Level::DEBUG)
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
+#[cfg(not(debug_assertions))]
+fn init_tracing() {
+    tracing_subscriber::fmt()
+        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
+        .with_span_events(FmtSpan::FULL)
+        .with_thread_names(true)
+        .init();
+}
+
 #[derive(Parser, Debug)]
 enum Op {
     #[command(subcommand)]
@@ -34,39 +69,4 @@ enum Alarm {
     ConnectRemote,
     ConnectLocal,
     Unlock,
-}
-
-#[cfg(debug_assertions)]
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
-        .with_max_level(tracing::Level::DEBUG)
-        .with_span_events(FmtSpan::FULL)
-        .with_thread_names(true)
-        .init();
-}
-
-#[cfg(not(debug_assertions))]
-fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_timer(ChronoLocal::new("%m-%d %H:%M:%S".into()))
-        .with_span_events(FmtSpan::FULL)
-        .with_thread_names(true)
-        .init();
-}
-
-// 参见：https://github.com/tokio-rs/tracing/issues/3068
-fn init_tracing_with_ansi() {
-    #[cfg(windows)]
-    nu_ansi_term::enable_ansi_support().ok();
-    init_tracing();
-}
-
-#[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
-    init_tracing_with_ansi();
-
-    match Op::init() {
-        _ => Err(anyhow::anyhow!("暂时todo！")),
-    }
 }
