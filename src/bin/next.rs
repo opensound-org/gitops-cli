62fd0aa3ae18f73d1bcd02198524b6cc36b8391b
29 <791603901@qq.com>
2024-09-02 05:27:22 +0800

    upgrade_hugo

--- src/bin/next.rs
+++ src/bin/next.rs
@@ -1,8 +1,14 @@
 use clap::{Parser, Subcommand, ValueEnum};
 use pushover_rs::{send_pushover_request, PushoverSound};
 use serde::Deserialize;
-use std::{env, fmt::Display};
-use tokio::fs;
+use std::{
+    env::{self, current_exe},
+    ffi::OsString,
+    fmt::Display,
+    io::Read,
+    path::PathBuf,
+};
+use tokio::{fs, process::Command};
 use tracing_subscriber::fmt::{format::FmtSpan, time::ChronoLocal};
 
 #[tokio::main]
@@ -19,13 +25,14 @@ async fn main() -> Result<(), anyhow::Error> {
             .await?;
     }
 
-    let _config = cli
+    let config = cli
         .resolve_config()
         .await
         .hook_err_if(op.is_deploy(), &pushover)
         .await?;
 
     match op {
+        Op::Upgrade(Target::Hugo) => upgrade_hugo(&config).await.map(|_| ()),
         _ => Err(anyhow::anyhow!("暂时todo！")),
     }
 }
@@ -169,7 +176,14 @@ impl Pushover {
 }
 
 #[derive(Deserialize)]
-struct Config;
+struct Config {
+    hugo: Option<HugoConfig>,
+}
+
+#[derive(Deserialize)]
+struct HugoConfig {
+    version: String,
+}
 
 trait HookErrIf<T>: Sized {
     async fn hook_err_if(self, predicate: bool, args: &T) -> Self {
@@ -198,4 +212,156 @@ impl<T, E: Display> HookErrIf<Pushover> for Result<T, E> {
             .ok();
         }
     }
+}
+
+async fn upgrade_hugo(config: &Config) -> Result<PathBuf, anyhow::Error> {
+    let version = &config
+        .hugo
+        .as_ref()
+        .ok_or(anyhow::anyhow!("找不到[hugo]字段！"))?
+        .version;
+
+    tracing::info!("请求的hugo版本是：{}", version);
+    tracing::info!("正在校验现有hugo版本……");
+
+    let exe = current_exe()?;
+    let hugo = exe.with_file_name("hugo");
+    let mut need_fetch = true;
+
+    if let Ok(output) = Command::new(&hugo).arg("version").output().await {
+        let status = output.status;
+
+        if status.success() {
+            if output
+                .stdout
+                .starts_with(format!("hugo v{}", version).as_bytes())
+            {
+                need_fetch = false;
+                tracing::info!("现有hugo版本匹配！将跳过下载");
+            } else {
+                tracing::info!("现有hug版本不匹配，准备更新hugo");
+            }
+        } else {
+            return Err(anyhow::anyhow!(
+                "hugo version执行失败！退出码：{}",
+                if let Some(code) = status.code() {
+                    code.to_string()
+                } else {
+                    "None".into()
+                }
+            ));
+        }
+    } else {
+        tracing::info!("hugo不存在，准备下载hugo");
+    }
+
+    if need_fetch {
+        #[cfg(target_os = "macos")]
+        const SUFFIX: &str = "darwin-universal.tar.gz";
+        #[cfg(target_os = "linux")]
+        const SUFFIX: &str = "Linux-64bit.tar.gz";
+        #[cfg(target_os = "windows")]
+        const SUFFIX: &str = "windows-amd64.zip";
+
+        let url = format!(
+            "https://github.com/gohugoio/hugo/releases/download/v{}/hugo_extended_{}_{}",
+            version, version, SUFFIX
+        );
+        tracing::info!("正在GET：{}", url);
+
+        let bytes = reqwest::get(url).await?.error_for_status()?.bytes().await?;
+
+        if bytes.is_empty() {
+            return Err(anyhow::anyhow!("未下载任何内容！"));
+        } else {
+            tracing::info!(
+                "已下载：{} MB",
+                retain_decimal_places(bytes.len() as f64 / 1024.0 / 1024.0, 3)
+            );
+            tracing::info!("正在解压……");
+
+            let (name, contents) = unzip(&bytes)?;
+            tracing::info!(
+                "正在保存：{:?}（{} MB）",
+                name,
+                retain_decimal_places(contents.len() as f64 / 1024.0 / 1024.0, 3)
+            );
+
+            let path = exe.with_file_name(name);
+            fs::write(&path, contents).await?;
+
+            #[cfg(not(windows))]
+            chmod_exec(path).await?;
+        }
+    }
+
+    Ok(hugo)
+}
+
+fn retain_decimal_places(f: f64, n: i32) -> f64 {
+    let power = 10.0f64.powi(n);
+    (f * power).round() / power
+}
+
+#[cfg(windows)]
+fn unzip(z: &[u8]) -> Result<(OsString, Vec<u8>), anyhow::Error> {
+    use std::io::Cursor;
+    use zip::ZipArchive;
+
+    let mut archive = ZipArchive::new(Cursor::new(z))?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let path = file
+            .enclosed_name()
+            .ok_or(anyhow::anyhow!("压缩文件路径异常！"))?;
+        let name = path
+            .file_name()
+            .ok_or(anyhow::anyhow!("压缩文件名异常！"))?;
+
+        if name
+            .to_str()
+            .ok_or(anyhow::anyhow!("压缩文件名编码异常！"))?
+            .starts_with("hugo")
+        {
+            let mut contents = Vec::new();
+            file.read_to_end(&mut contents)?;
+            return Ok((name.to_owned(), contents));
+        }
+    }
+
+    Err(anyhow::anyhow!("压缩包中未找到hugo执行文件！"))
+}
+
+#[cfg(not(windows))]
+fn unzip(z: &[u8]) -> Result<(OsString, Vec<u8>), anyhow::Error> {
+    use flate2::read::GzDecoder;
+    use tar::Archive;
+
+    for entry in Archive::new(GzDecoder::new(z)).entries()? {
+        let mut file = entry?;
+        let path = file.path()?.into_owned();
+        let name = path
+            .file_name()
+            .ok_or(anyhow::anyhow!("压缩文件名异常！"))?;
+
+        if name
+            .to_str()
+            .ok_or(anyhow::anyhow!("压缩文件名编码异常！"))?
+            .starts_with("hugo")
+        {
+            let mut contents = Vec::new();
+            file.read_to_end(&mut contents)?;
+            return Ok((name.to_owned(), contents));
+        }
+    }
+
+    Err(anyhow::anyhow!("压缩包中未找到hugo执行文件！"))
+}
+
+#[cfg(not(windows))]
+async fn chmod_exec(path: impl AsRef<std::path::Path>) -> Result<(), anyhow::Error> {
+    tracing::info!("正在设置执行权限……");
+    use std::{fs::Permissions, os::unix::prelude::PermissionsExt};
+    Ok(fs::set_permissions(path, Permissions::from_mode(0o755)).await?)
 }
